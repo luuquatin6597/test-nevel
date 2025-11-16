3c6a401e1a89379729f937d686072f250f4552a0
Luu Quang Tinh <luuquangtinh6597@gmail.com>
2025-11-16 12:36:48 +0700

    Fix basePath config for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/test-nevel" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/test-nevel" : "",
+  basePath: "/test-nevel",
+  assetPrefix: "/test-nevel",
 };
 
 export default nextConfig;
